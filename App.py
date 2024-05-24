59de6e687512feb409961949b69f089b36b0460c
filatov david <filatovdavid62@gmail.com>
2024-05-24 15:35:59 +0300

    Коммит-4. Написан функционал для получения данных из формы

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 
 App = Flask(__name__)
@@ -6,7 +6,12 @@ App = Flask(__name__)
 
 @App.route("/", methods=["GET", "POST"])
 def main_page():
-    return render_template("query.html")
+    if request.method == "POST":
+        text = request.form["main_field"]
+
+        return render_template("query.html")
+    else:
+        return render_template("query.html")
 
 
 if __name__ == "__main__":
